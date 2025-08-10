7c250629fd4158bd5ab9e1f53539f3f79e901600
code-with-vasu <agvasu2015@gmail.com>
2025-08-10 23:52:12 +0530

    again ido static changes

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -117,10 +117,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+
+# Base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# This is where collectstatic will put all files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: If you have additional static folders
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
